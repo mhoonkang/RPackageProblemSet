9a1aace4523adb70dfc5c86b29721046eec461b0
mhoonkang <myunghoon@wustl.edu>
2014-03-14 17:23:05 -0500

    add package

---
+++ PolyregPack/R/PolyregPack-package.R
@@ -0,0 +1,14 @@
+#' PolyregPack
+#'
+#' The PolyregPack package performs regression for all possible combinations of covariates.  
+#' @name PolyregPack
+#' @docType package
+#' @author  Myunghoon Kang: \email{myunghoon@@wustl.edu}
+#' @examples 
+#'
+#' \dontrun{
+#' demo(reg)
+#' demo(signi)
+#' }
+#'
+NULL
